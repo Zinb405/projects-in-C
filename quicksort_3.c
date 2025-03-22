185994be491f40e495b06c8ca166994de9f53217
Zinb405 <118635654+Zinb405@users.noreply.github.com>
2025-03-22 13:20:52 +0100

    quicksort_3.c

---
+++ quicksort_3.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+long count = 0;
+
+int partition(int array[], int len);
+int median_of_three(int left,int mid,int right);
+
+int median_of_three(int left,int mid,int right)
+{
+   int pivot=0;
+   if ((left < mid && left > right) || (left > mid && left < right))
+   {
+    pivot = left;
+   }
+   else if ((mid < left && mid > right) || (mid > left && mid < right))
+   {
+    pivot = mid;
+   }
+   else
+   {
+    pivot = right;
+   }
+    return pivot;
+}
+int partition(int array[], int len)
+{
+    // base case
+    if (len <= 1)
+    {
+        return count;
+    }
+    int j, k, p, tmp;
+    int i = 1;
+    int pivot;
+    int mid;
+    int length;
+    // pick up the  pivot
+     if(len % 2 == 0)
+     {
+       mid = array[(len/2) - 1];
+     }
+     else
+     {
+        mid = array[len/2];
+     }
+     int left = array[0];
+     int right = array[len-1];
+     pivot = median_of_three(left,mid,right);
+
+    //change the pivot with first element
+    if (pivot == mid && len % 2 == 0)
+    {
+        tmp=array[0];
+        array[0]=pivot;
+        array[(len/2) - 1]=tmp;
+    }
+    if (pivot == mid && len % 2 != 0)
+    {
+        tmp=array[0];
+        array[0]=pivot;
+        array[len/2]=tmp;
+    }
+    if (pivot == right)
+    {
+        tmp=array[0];
+        array[0]=pivot;
+        array[len-1]=tmp;
+    }
+    // comparison
+    for (j = 1; j < len; j++)
+    {
+
+        if (array[j] < pivot)
+        {
+            tmp = array[j];
+            array[j] = array[i];
+            array[i] = tmp;
+            i++;
+        }
+        count++;
+    }
+    // swap pivot
+    tmp = array[i - 1];
+    array[i - 1] = pivot;
+    array[0] = tmp;
+    length = i - 1;
+    int *less_pivot = (int *) malloc(length * sizeof(int));
+    if (less_pivot == NULL)
+    {
+
+        exit(1);
+    }
+    int *great_pivot = (int *) malloc((len - length - 1) * sizeof(int));
+    if (great_pivot == NULL)
+    {
+
+        exit(1);
+    }
+    // copy elements into the left partition
+    for (k = 0; k < length; k++)
+    {
+        less_pivot[k] = array[k];
+    }
+    // copy elements into the right partition
+    for (k = 0, p = length + 1; k < len - length - 1; k++, p++)
+    {
+
+        great_pivot[k] = array[p];
+    }
+    // recursive calls
+    partition(less_pivot, length);
+    partition(great_pivot, len - length - 1);
+    // free allocated memory
+    free(less_pivot);
+    free(great_pivot);
+
+    return count;
+}
+
+int main(int argc, char *argv[])
+{
+
+    if (argc != 2)
+    {
+        printf("Enter please the text file\n");
+    }
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+
+        printf("File coudn't be open!\n");
+        return 1;
+    }
+    int len = 10000;
+    int array[len];
+
+    for (int i = 0; i < len; i++)
+    {
+        fscanf(input, "%d", &array[i]);
+    }
+    partition(array, len);
+
+    printf("%ld\n", count);
+
+    fclose(input);
+    return 0;
+}
